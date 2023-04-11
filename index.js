78f747575dba5892b2111d162ea6512f7b8b9101
Xanul <rivas.060@gmail.com>
2023-04-10 20:43:53 -0600

    Added Searched class in /models and added the main structure of case 1 (city search) option

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 const { readInput, inquirerMenu, pause } = require("./helpers/inquirer");
+const Searches = require("./models/searches");
 
 
 const main = async () => {
 
   let menuOption;
+  const searchedTerms = new Searches();
+  
 
   do {
 
     menuOption = await inquirerMenu();
     console.log(menuOption);
+    
+    switch (menuOption) {
+      case 1:
+        // Mostrar mensaje
+        const place = await readInput("Please enter a city");
+        console.log(place);
+        // Buscar el lugar
+        // Seleccionar el lugar 
+        // Datos del clima
+        // Mostrar los resultados
+        console.log("\nCity Information\n".green);
+        console.log("City:", );
+        console.log("Lat:", );
+        console.log("Lng:", );
+        console.log("Temperature:", );
+        console.log("Min Temp:", );
+        console.log("Max Temp:", );
+        break;
+    
+      default:
+        break;
+    }
+    
 
     if (menuOption !== 0) await pause();
     
   } while (menuOption !== 0);
+
+
+
 
 }
 
---
+++ models/searches.js
@@ -0,0 +1,28 @@
+
+
+class Searches {
+
+  history = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+  // Constructor
+  constructor() {
+    //TODO: Leer base de datos si existe
+
+
+
+  }
+
+  // Methods
+  async city( place = '' ) {
+
+    // Peticion HTTP
+    console.log(place);
+
+    return []; // Retornar los lugares que coincidan con la busqueda
+
+  }
+
+
+}
+
+module.exports = Searches;
